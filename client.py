7b0cbf7e5d6e8224674e9e75da1d46fda7e24db5
iortego42 <iortego42madrid@gmail.com>
2023-01-09 14:41:33 +0100

    creación del cliente para pruebas

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import socket
+import sys
 
-host = socket.gethostname()
-port = 10042
+host = '127.0.0.1'
+port = 4444
 BUFFER_SIZE = 1024
 MESSAGE = 'Hola, mundo!'
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
@@ -9,3 +10,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
     # Convertimos str a bytes
     socket_tcp.send(MESSAGE.encode('utf-8'))
     data = socket_tcp.recv(BUFFER_SIZE)
+    print(data.decode('utf-8'))
--- talon.py
+++ talon.py
@@ -1,11 +1,42 @@
 import socket
+from colored import fg, bg, attr
+import ipaddress
+import argparse
 
-host = socket.gethostname()
-port = 10042
+PORT_MAX_VALUE = 65535
+host = '0.0.0.0'
+port = 4444
+
+
+def parse_arguments():
+    global host
+    global port
+    parser = argparse.ArgumentParser(prog="Talon")
+    parser.add_argument('host')
+    parser.add_argument('port', type=int,)
+    parser.add_argument('-c', '--count', required=False, )
+    args = parser.parse_args()
+    print(args)
+    try:
+        lhost = ipaddress.ip_address(args.host)
+        if not lhost.is_private:
+            print(f"{bg('yellow_1')}{fg('red_3a')} Warning! {attr(0)} This IP Address is not a private one")
+            exit()
+        if args.port > PORT_MAX_VALUE or args.port < 0:
+            print(f"{fg('red')} ERROR: {attr(0)}This is a not valid port")
+            exit()
+    except ValueError:
+        print(f"""{fg('red')} ERROR: {attr(0)}Invalid input. Example:
+python3 talon.py {fg('grey_19')}{bg('gold_3b')} host {attr(0)} {fg('grey_19')}{bg('cornflower_blue')} port {attr(0)}""")
+        exit()
+    
+
+parse_arguments()
+addr_info = socket.getaddrinfo(host, port, type=socket.AF_INET)[0]
 BUFFER_SIZE = 1024
 
 session = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-session.bind((host, port))
+session.bind(addr_info[-1])
 session.listen(5)
 conn, remote_addr = session.accept()
 with conn:
@@ -13,7 +44,7 @@ with conn:
         print("Conexion establecida con:", remote_addr)
         data = conn.recv(BUFFER_SIZE)
         if not data:
-            break    
+            break
         else:
             print("Datos recibidos", data.decode('utf-8'))
         conn.send(data)
